221bdb503551c67961e4843737db38a12b8f050d
Vijay Ganduri <vijayganduri@gmail.com>
2015-09-14 15:11:30 +0530

    using https calls as the previous http is redirecting to https

--- NutriCheck/app/src/main/java/com/vijayganduri/nutricheck/Config.java
+++ NutriCheck/app/src/main/java/com/vijayganduri/nutricheck/Config.java
@@ -4,6 +4,6 @@ package com.vijayganduri.nutricheck;
 public class Config {
 	
 	public static final String BUGSENSE_KEY = "779467b8";
-	public static final String BASE_URL = "http://test.holmusk.com";
+	public static final String BASE_URL = "https://test.holmusk.com";
 	
 }
